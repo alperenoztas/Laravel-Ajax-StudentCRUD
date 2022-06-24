436f608917ede15d6d0326e4a4bc1e8694c24029
Alperen Öztaş <alprnoztas@gmail.com>
2022-06-24 09:23:25 +0300

    Student model fillable section and table link set

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     use HasFactory;
+
+    protected $table = 'students';
+
+    protected $fillable = [
+        'student_id',
+        'first_name',
+        'last_name',
+        'email',
+        'phone',
+        'avatar',
+    ];
 }
