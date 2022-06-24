f6c354a5fe1d72cfeeba5adcbdd775363c6bbe12
Alperen Öztaş <alprnoztas@gmail.com>
2022-06-24 09:21:25 +0300

    Table data has set

--- database/migrations/2022_06_24_061541_create_students_table.php
+++ database/migrations/2022_06_24_061541_create_students_table.php
@@ -15,6 +15,12 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string('student_id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('avatar');
             $table->timestamps();
         });
     }
